564311d5d647e35cc31cc4aaf7fb0bc42fcf648f
Sam Schoenberg <sam4130@Sams-MacBook-Pro.local>
2023-01-29 13:38:04 -0700

    inital setup

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -1,11 +1,33 @@
+const { ethers } = require("hardhat");
+
 async function main() {
+  console.log(`Preparing deployment..... \n`) // test to insure that deployment is kicked off
 //script to manage token 
     //fetch contract to deploy
     const Token = await ethers.getContractFactory('Token');
-    // deploy contract
-    const token = await Token.deploy();
-    await token.deployed();
-    console.log(`Token Deployed to: ${token.address}`); //test
+    const Exchange = await ethers.getContractFactory('Exchange');
+    
+  const accounts = await ethers.getSigners() // deploy with fee account
+
+  console.log(`Accounts fetched:\n${accounts[0].address}\n${accounts[1].address}`)
+
+    // deploy contracts
+    const DApp = await Token.deploy('THE GREEK AND GLOVER', 'PETE THE GREEK', '1000000');
+    await DApp.deployed();
+    console.log(`PETE THE GREEK Deployed to: ${DApp.address}`); //DApp coin
+
+    const mETH = await Token.deploy('mETH', 'mETH', '1000000');
+    await mETH.deployed();
+    console.log(`mETH Deployed to: ${mETH.address}`); //meth coin
+
+    const mDAI = await Token.deploy('mDAI', 'mDAI', '1000000');
+    await mDAI.deployed();
+    console.log(`mDAI Deployed to: ${mDAI.address}`); //dai coin
+
+    const exchange = await Exchange.deploy(accounts[1].address, 10)
+    await exchange.deployed()
+    console.log(`Exchange Deployed to: ${exchange.address}`) // DApp exchange 
+
 }
 // boiler plate execute code
 main()
---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import '../App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <p>This must be the place!!</p>
+    </div>
+  );
+}
+
+export default App;
